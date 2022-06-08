58dd146e5caad92c4a50803e1e15ed5466b09e6e
Sneha Mahajan <sneh.km@gmail.com>
2022-06-07 20:49:37 -0400

    Add tables.

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -29,14 +29,14 @@ CREATE TABLE properties (
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
-  active BOOLEAN NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
-  start_date DATE,
-  end_date DATE,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
@@ -44,7 +44,7 @@ CREATE TABLE reservations (
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
-  rating SMALLINT,
+  rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
